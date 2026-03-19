fa15808e76a7d40166fcad842795424e66547a9a
student-yashsingh <yash1243singh@gmail.com>
2026-03-19 17:27:00 +0530

    learning how to give requests

--- phlaphlapyaar/fst_project/urls.py
+++ phlaphlapyaar/fst_project/urls.py
@@ -15,13 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from . import views
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('',views.home,name='home'),
-    path('about/',views.about,name='about'),
-    path('contact/',views.contact,name='about'),
-    path('fst_project/',include('fst_project.urls'))
+    path('',views.fst,name='fst')
 ]
---
+++ phlaphlapyaar/fst_project/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def fst(request): 
+    return render(request,'fst_project/fst.html')
